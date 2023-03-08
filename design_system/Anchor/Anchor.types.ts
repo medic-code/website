7e0e210e426593b980ba0f59099b84bf1223e0a9
Aaron Smith <u40as7@gmail.com>
2023-03-08 11:30:49 +0000

    Fix typescript errors for Anchor comp

--- design_system/Anchor/Anchor.types.ts
+++ design_system/Anchor/Anchor.types.ts
@@ -2,7 +2,7 @@ interface UnderlineAnchorProps
   extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   children: React.ReactNode;
   modifiers?: string[] | 'underline';
-  size: string;
+  size?: string;
 }
 
 export default UnderlineAnchorProps;
