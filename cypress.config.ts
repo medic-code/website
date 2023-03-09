d0dcd76c59a5980a03f3a8b79dd3199357f37862
Aaron Smith <u40as7@gmail.com>
2023-03-09 17:14:56 +0000

    Add initial cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,13 +3,11 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   chromeWebSecurity: false,
   video: false,
+
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
+      // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
   },
   experimentalWebKitSupport: true,
 });
