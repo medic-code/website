c6d7e2b577a955b1b5a2be488dfbf2a67519d842
Aaron Smith <u40as7@gmail.com>
2023-03-10 09:48:27 +0000

    Add project page details + add to design sytem to make more responsive

--- pages/iolassist.tsx
+++ pages/iolassist.tsx
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 
-import { LogoIcon, Typography, fontSizes, Flex } from '@/design_system';
+import {
+  LogoIcon,
+  Typography,
+  fontSizes,
+  Flex,
+  Anchor,
+  BackArrow,
+} from '@/design_system';
 import Layout from '../core/Layout/index';
 import Grid from '../design_system/Grid/index';
 import iolassist from '../public/Images/1.svg';
 import iolassist1 from '../public/Images/2.svg';
+import Link from 'next/link';
 
 const IconPadding = styled.div`
   padding-top: 20px;
@@ -16,15 +24,29 @@ const IolAssist = () => {
     <Layout footer>
       <Grid column="1fr minmax(auto,70ch) 1fr" gap="0px 16px">
         <IconPadding>
-          <LogoIcon />
-          <Typography modifiers="heading1" size={fontSizes['--header1']}>
-              IOL Assist
-            </Typography>
+          <Flex flexDirection="column" alignItems="flex-start" gap="32px">
+            <Anchor href="/">
+              <LogoIcon />
+            </Anchor>
+            <Anchor href="/">
+              <Flex alignItems="center">
+                <BackArrow />
+                Home
+              </Flex>
+            </Anchor>
+          </Flex>
+          <Typography
+            margin="16px 0px 0px 0px"
+            fontWeight="var(--bold)"
+            size={fontSizes['--header1']}
+            color="hsl(var(--palette-green-75))"
+          >
+            IOL Assist
+          </Typography>
         </IconPadding>
-       
+
         <article>
           <section>
-          
             <Typography size={fontSizes['--paragraph']}>
               A mobile app to support cataract surgeons with intra-ocular lens
               choices
@@ -53,7 +75,11 @@ const IolAssist = () => {
             </Flex>
           </section>
           <section>
-            <Typography margin="16px 0 0 0" modifiers="heading2" size={fontSizes['--header2']}>
+            <Typography
+              margin="16px 0px 0px 0px"
+              fontWeight="var(--semibold)"
+              size={fontSizes['--header2']}
+            >
               The Project
             </Typography>
             <Typography size={fontSizes['--paragraph']}>
