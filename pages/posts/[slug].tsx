85b07e86d1545b5b7c7e41069e308160a8253361
Aaron Smith <u40as7@gmail.com>
2023-03-14 08:36:57 +0000

    Add dynamic routing for blog posts

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,43 @@
+import { useRouter } from 'next/router';
+import { MDXRemote } from 'next-mdx-remote';
+
+import { getFiles, getFileBySlug } from '@/lib/mdx';
+import MDXComponents from '@/core/components/MDX/MDXComponents';
+
+const Blog = ({ post }) => {
+  const { isFallback } = useRouter();
+
+  if (isFallback || !post) {
+    return <div>Loading....</div>;
+  }
+
+  return (
+    <div>
+      <MDXRemote {...post.mdxSource} components={{ ...MDXComponents }} />
+    </div>
+  );
+};
+
+export default Blog;
+
+export const getStaticPaths = async () => {
+  const posts = await getFiles();
+
+  return {
+    paths: posts.map((post) => ({
+      params: {
+        slug: post.replace(/\.mdx/, ''),
+      },
+    })),
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  try {
+    const post = await getFileBySlug(params.slug);
+    return { props: { post } };
+  } catch (error) {
+    return { notFound: true };
+  }
+};
---
+++ types/post.ts
@@ -0,0 +1,9 @@
+export type Post = {
+  date: string;
+  slug: string;
+  title: string;
+  subtitle: string;
+  updated: string;
+  keywords?: string[];
+  categories?: string[];
+};
