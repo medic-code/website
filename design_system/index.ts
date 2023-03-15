b7ff1074db54e5a80e82408e38ec70dec8a389e1
Aaron Smith <u40as7@gmail.com>
2023-03-15 11:14:26 +0000

    Add List comp to index of design system

--- design_system/index.ts
+++ design_system/index.ts
@@ -8,5 +8,6 @@ export { default as Typography } from './Typography';
 export { default as Grid } from './Grid';
 export { default as Flex } from './Flex';
 export { default as Box } from './Box';
+export { default as List } from './List';
 
 export * from './Icon';
