7655b13c4d7a5126da6efe1e494dcb794d88db83
Aaron Smith <u40as7@gmail.com>
2023-03-15 11:12:39 +0000

    Add WIP List Comp

---
+++ design_system/List/styles.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const StyledListItem = styled.li`
+  list-style: none;
+  display: flex;
+  marginbottom: 1rem;
+  padding: 0;
+  lineheight: 1.9;
+  letter-spacing: 0.3px;
+`;
+
+export const StyledUnorderedList = styled.ul`
+  list-style-type: none;
+`;
+export const StyledOrderedList = styled.ol`
+  list-style-type: decimal;
+`;
