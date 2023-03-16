695a1eb3fb828c64192d8585a62ee77a8ac33d15
Aaron Smith <u40as7@gmail.com>
2023-03-16 10:17:03 +0000

    Add blog layout styles

---
+++ core/BlogLayout/BlogLayout.tsx
@@ -0,0 +1,23 @@
+import Layout from '../Layout/index';
+import { Post } from '@/types/post';
+import { Grid } from '@/design_system/index';
+import { Hero } from '../components/Hero/Hero';
+
+interface Props {
+  children: React.ReactNode;
+  frontMatter: Post;
+}
+
+const BlogLayout = ({ children, frontMatter }: Props) => {
+  const { date, title, lastUpdated } = frontMatter;
+  return (
+    <Layout footer>
+      <Grid column="1fr minmax(auto,70ch) 1fr">
+        <Hero title={title} date={date} lastUpdated={lastUpdated} />
+        <article>{children}</article>
+      </Grid>
+    </Layout>
+  );
+};
+
+export default BlogLayout;
