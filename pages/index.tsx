a4c06156404631018c28fb70ae0e31355aec3956
Aaron Smith <u40as7@gmail.com>
2023-03-20 16:16:55 +0000

    Fix hover color change for date in blog post

--- pages/index.tsx
+++ pages/index.tsx
@@ -69,7 +69,6 @@ const StyledAnchor = styled(Anchor)`
 const StyledDate = styled(Typography)`
   margin: 0px;
   font-size: var(--paragraph);
-  color: hsl(var(--palette-grey-25));
   min-width: 60px;
 `;
 
