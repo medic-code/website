d4c14073ca4b3308ea932e50fd6f5f4676b4b8bb
Aaron Smith <u40as7@gmail.com>
2023-03-24 20:12:29 +0000

    Fix image styling to center and reduce size of images

--- core/components/MDX/Image.tsx
+++ core/components/MDX/Image.tsx
@@ -4,15 +4,24 @@ import { Typography, fontSizes } from '@/design_system';
 
 const StyledFigure = styled.figure`
   margin-bottom: 1rem;
+  display: flex;
+  justify-content: center;
 `;
 
+const ImageStyle = styled.div`
+  margin: auto;
+  max-width: 70%;
+  height: auto;
+`;
 const MDXImage = (props: ImageProps) => {
   return (
     <>
-      <Image {...props} quality={70} alt={props.alt} />
-      <StyledFigure>
-        <Typography size={fontSizes['--paragraph']}>{props.alt}</Typography>
-      </StyledFigure>
+      <ImageStyle>
+        <Image {...props} quality={70} alt={props.alt} />
+        <StyledFigure>
+          <Typography size={fontSizes['--paragraph']}>{props.alt}</Typography>
+        </StyledFigure>
+      </ImageStyle>
     </>
   );
 };
